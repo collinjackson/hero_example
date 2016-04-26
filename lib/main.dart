6766bdb12d0038f662858d22971f6799b8ee45b0
Collin Jackson <jackson@google.com>
2016-04-26 13:56:52 -0700

    Example of page hero transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,42 +9,128 @@ void main() {
       theme: new ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: new FlutterDemo()
+      home: new ListPage()
     )
   );
 }
 
-class FlutterDemo extends StatefulWidget {
-  FlutterDemo({ Key key }) : super(key: key);
+Key _apple = new Key('apple');
+
+const Duration kHeroTransitionDuration = const Duration(milliseconds: 600);
+
+class HeroTransitionRoute extends MaterialPageRoute<Null> {
+  HeroTransitionRoute({
+    WidgetBuilder builder,
+    RouteSettings settings: const RouteSettings()
+  }) : super(builder: builder, settings: settings);
+
+  // Cause the hero image to animate in under the toolbar
+  @override
+  void insertHeroOverlayEntry(OverlayEntry entry, Object tag, OverlayState state) {
+    state.insert(entry, above: overlayEntries.first);
+  }
 
   @override
-  _FlutterDemoState createState() => new _FlutterDemoState();
+  Duration get transitionDuration => kHeroTransitionDuration;
+
+  // Don't shift the page up as we transition to it
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> forwardAnimation, Widget child) {
+    return new FadeTransition(
+      opacity: animation,
+      child: child
+    );
+  }
 }
 
-class _FlutterDemoState extends State<FlutterDemo> {
-  int _counter = 0;
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: [
+        new Positioned(
+          top: kToolBarHeight + ui.window.padding.top,
+          bottom: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: new Hero(
+            tag: 'fruit',
+            key: _apple,
+            child: new Container(
+              decoration: new BoxDecoration(
+                backgroundColor: Colors.orange[500]
+              ),
+              child: new Center(
+                child: new Text('This is some detail')
+              )
+            )
+          )
+        ),
+        new Positioned(
+          top: 0.0, right: 0.0, left: 0.0,
+          child: new SizedBox(
+            height: kToolBarHeight + ui.window.padding.top,
+            child: new AppBar(
+              title: new Text('Detail view'),
+              leading:
+                Navigator.canPop(context) ?
+                new IconButton(
+                  icon: Icons.arrow_back,
+                  alignment: FractionalOffset.centerLeft,
+                  onPressed: () => Navigator.pop(context),
+                  tooltip: 'Back' // TODO(ianh): Figure out how to localize this string
+                ) :
+                null
+            )
+          )
+        )
+      ]);
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class ListPage extends StatefulWidget {
+  ListPage({ Key key }) : super(key: key);
+
+  @override
+  _ListPageState createState() => new _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+
+  void _onTap(BuildContext context, Key key) {
+    Navigator.push(
+      context,
+      new HeroTransitionRoute(
+        builder: (_) => new DetailPage(),
+        settings: new RouteSettings(
+          mostValuableKeys: new Set<Key>.from([key])
+        )
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Flutter Demo')
+        title: new Text('List view')
       ),
-      body: new Center(
-        child: new Text('Button tapped $_counter time${ _counter == 1 ? '' : 's' }.')
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(
-          icon: Icons.add
-        )
+      body: new Block(
+        children: [
+          new ListItem(
+            onTap: () => _onTap(context, _apple),
+            title: new Hero(
+              tag: 'fruit',
+              key: _apple,
+              child: new Container(
+                decoration: new BoxDecoration(
+                  backgroundColor: Colors.orange[500]
+                ),
+                child: new Text('element')
+              )
+            )
+          )
+        ]
       )
     );
   }
